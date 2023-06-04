dfb22d1948346e6a198cdadabb43dc923546d20c
yshuai <58702199+stefan-ysh@users.noreply.github.com>
2023-06-04 17:51:55 +0800

    feat:add global function $message $dialog

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,6 +4,9 @@ import { useLang, useTheme } from "@/hooks";
 import { NConfigProvider, GlobalThemeOverrides } from "naive-ui";
 import { useSettingStore } from "@/stores/setting";
 import { I18n } from '@/components/I18n'
+import { NMessageProvider, NDialogProvider } from "naive-ui";
+// 引入上面你所定义的全局挂载组件
+import GlobalContainer from "@/layout/global.vue";
 const setting = useSettingStore();
 setting.init();
 // 全局语言
@@ -16,6 +19,10 @@ const { theme } = useTheme();
   <n-config-provider :theme="theme" :locale="locale" :date-locale="dateLocale">
     <I18n></I18n>
     <!-- <Home /> -->
-    <router-view></router-view>
+    <n-message-provider>
+        <n-dialog-provider>
+            <global-container />
+        </n-dialog-provider>
+    </n-message-provider>
   </n-config-provider>
 </template>
---
+++ frontend/src/layout/global.vue
@@ -0,0 +1,13 @@
+<template>
+    <router-view />
+</template>
+
+<script lang="ts" setup>
+import { useDialog, useMessage } from "naive-ui";
+
+// 将 dialog & message 实例方法挂载
+// $message 和 $dialog 随个人喜好定义名称
+// 注意不要太过于简单，以免污染其他全局定义
+window.$message = useMessage();
+window.$dialog = useDialog();
+</script>
